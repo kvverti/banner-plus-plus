7253da705220043e6a4c4e8935eda1f2600372c8
Chris Nero <chrisvn00@gmail.com>
2019-05-07 00:35:08 -0400

    Add interior forth banner patterns

--- src/main/java/io/github/kvverti/bannerpp/NewBannerPatterns.java
+++ src/main/java/io/github/kvverti/bannerpp/NewBannerPatterns.java
@@ -14,6 +14,10 @@ public class NewBannerPatterns implements Runnable {
             .addEnum("BANNERPP_STRIPE_CENTER_RIGHT_FIFTH", "bannerpp_stripe_center_right_fifth", "bpp_crf", null, null, null)
             .addEnum("BANNERPP_STRIPE_MIDDLE_UPPER_FIFTH", "bannerpp_stripe_middle_upper_fifth", "bpp_muf", null, null, null)
             .addEnum("BANNERPP_STRIPE_MIDDLE_LOWER_FIFTH", "bannerpp_stripe_middle_lower_fifth", "bpp_mof", null, null, null)
+            .addEnum("BANNERPP_STRIPE_CENTER_LEFT_FORTH", "bannerpp_stripe_center_left_forth", "bpp_cl4", null, null, null)
+            .addEnum("BANNERPP_STRIPE_CENTER_RIGHT_FORTH", "bannerpp_stripe_center_right_forth", "bpp_cr4", null, null, null)
+            .addEnum("BANNERPP_STRIPE_MIDDLE_UPPER_FORTH", "bannerpp_stripe_middle_upper_forth", "bpp_mu4", null, null, null)
+            .addEnum("BANNERPP_STRIPE_MIDDLE_LOWER_FORTH", "bannerpp_stripe_middle_lower_forth", "bpp_mo4", null, null, null)
             .build();
         // special banner patterns
         ClassTinkerers.enumBuilder(bannerPatternClass, String.class, String.class)
