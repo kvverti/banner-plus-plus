16d018da81ec0dd19821e1b60d26d18d5444d2b4
Chris Nero <chrisvn00@gmail.com>
2019-05-12 22:33:10 -0400

    Refactor MixinLoomContainer to remove Overwrite

--- src/main/java/io/github/kvverti/bannerpp/mixin/MixinLoomContainer.java
+++ src/main/java/io/github/kvverti/bannerpp/mixin/MixinLoomContainer.java
@@ -7,16 +7,11 @@ import net.minecraft.container.Container;
 import net.minecraft.container.ContainerType;
 import net.minecraft.container.LoomContainer;
 import net.minecraft.container.Property;
-import net.minecraft.container.Slot;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.Inventory;
-import net.minecraft.item.BannerPatternItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.CompoundTag;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * Overwrites LoomContainer methods to correctly determine dye banner patterms.
@@ -29,65 +24,51 @@ public abstract class MixinLoomContainer extends Container {
         super(ct, i);
     }
 
-    // shadow members
-
-    @Shadow private Property selectedPattern;
-    @Shadow private Slot bannerSlot;
-    @Shadow private Slot dyeSlot;
-    @Shadow private Slot patternSlot;
-    @Shadow private Slot outputSlot;
-
-    @Shadow private void updateOutputSlot() {}
-
     // mixin members
 
     private static final BannerPattern[] patterns = BannerPattern.values();
 
     /**
-     * Sets the output slot based on which background index the client clicks.
+     * Replace the default dye banner pattern cutoff with our own.
      */
-    @Overwrite
-    public boolean onButtonClick(PlayerEntity player, int patternIdxPlus1) {
-        int patternIdx = patternIdxPlus1 - 1;
-        // replace conditional to check for dye pattern index instead of
-        // pattern ID
-        if(patternIdx >= 0 && patternIdx < LoomPattern.RECIPE_PATTERNS.size()) {
-            this.selectedPattern.set(LoomPattern.RECIPE_PATTERNS.get(patternIdx).ordinal());
-            this.updateOutputSlot();
-            return true;
-        } else {
-            return false;
-        }
+    @Redirect(
+        method = "onButtonClick",
+        at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/block/entity/BannerPattern;field_18283:I"
+        )
+    )
+    private int getBannerPatternCutoff() {
+        return LoomPattern.RECIPE_PATTERNS.size();
     }
 
     /**
-     * Determines the selected pattern and output slot on content change.
+     * Transform the dye banner pattern index into the actual ordinal
+     * that Minecraft expects
      */
-    @Overwrite
-    public void onContentChanged(Inventory inv) {
-        ItemStack banner = this.bannerSlot.getStack();
-        ItemStack dye = this.dyeSlot.getStack();
-        ItemStack pattern = this.patternSlot.getStack();
-        ItemStack output = this.outputSlot.getStack();
-        // replace comparing pattern ID to a hardcoded limit with directly
-        // querying the desired property (added via mixin)
-        boolean selectedNeedsItem = ((LoomPattern)(Object)patterns[this.selectedPattern.get()]).requiresPatternItem();
-        if (output.isEmpty() || !banner.isEmpty() && !dye.isEmpty() && this.selectedPattern.get() > 0 && (!selectedNeedsItem || !pattern.isEmpty())) {
-            if (!pattern.isEmpty() && pattern.getItem() instanceof BannerPatternItem) {
-                CompoundTag blockEntityTag = banner.getOrCreateSubCompoundTag("BlockEntityTag");
-                boolean bannerIsFull = blockEntityTag.containsKey("Patterns", 9) && !banner.isEmpty() && blockEntityTag.getList("Patterns", 10).size() >= 6;
-                if (bannerIsFull) {
-                    this.selectedPattern.set(0);
-                } else {
-                    this.selectedPattern.set(((BannerPatternItem)pattern.getItem()).getPattern().ordinal());
-                }
-            }
-        } else {
-            this.outputSlot.setStack(ItemStack.EMPTY);
-            this.selectedPattern.set(0);
-        }
+    @ModifyVariable(
+        method = "onButtonClick",
+        at = @At(value = "LOAD", ordinal = 2)
+    )
+    private int transformPatternIndex(int indexPlusOne) {
+        return LoomPattern.RECIPE_PATTERNS.get(indexPlusOne - 1).ordinal();
+    }
 
-        this.updateOutputSlot();
-        this.sendContentUpdates();
+    /**
+     * Replace the upper bound test for `this.selectedPattern.get()` with a test
+     * for our mixed in interface. Because the method expects an int, we return 0
+     * for true, and Integer.MAX_VALUE for false.
+     */
+    @Redirect(
+        method = "onContentChanged",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/container/Property;get()I",
+            ordinal = 1
+        )
+    )
+    private int isActuallySpecialPattern(Property self) {
+        boolean special = ((LoomPattern)(Object)patterns[self.get()]).requiresPatternItem();
+        return special ? Integer.MAX_VALUE : 0;
     }
 }
