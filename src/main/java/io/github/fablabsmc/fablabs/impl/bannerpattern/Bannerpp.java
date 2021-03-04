e036e4d6edeffe815695b9c7a436ac8380c2d89a
Thalia Nero <chrisvn00@gmail.com>
2021-03-04 16:12:21 -0500

    Add loom patterns to banner loot table drops.

--- src/main/java/io/github/fablabsmc/fablabs/impl/bannerpattern/Bannerpp.java
+++ src/main/java/io/github/fablabsmc/fablabs/impl/bannerpattern/Bannerpp.java
@@ -1,7 +1,12 @@
 package io.github.fablabsmc.fablabs.impl.bannerpattern;
 
-import io.github.fablabsmc.fablabs.api.bannerpattern.v1.LoomPattern;
+import java.util.Arrays;
+import java.util.List;
 
+import io.github.fablabsmc.fablabs.api.bannerpattern.v1.LoomPattern;
+import io.github.fablabsmc.fablabs.impl.bannerpattern.iface.LoomPatternContainer;
+
+import net.minecraft.loot.function.CopyNbtLootFunction;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -10,6 +15,7 @@ import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder;
 import net.fabricmc.fabric.api.event.registry.RegistryAttribute;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.fabricmc.fabric.api.event.registry.RegistryIdRemapCallback;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 
 public final class Bannerpp implements ModInitializer {
 	// metadata
@@ -19,6 +25,25 @@ public final class Bannerpp implements ModInitializer {
 			.createSimple(LoomPattern.class, new Identifier(MODID, "loom_patterns"))
 			.attribute(RegistryAttribute.SYNCED)
 			.buildAndRegister();
+
+	private static final List<Identifier> BANNER_LOOT_TABLES = Arrays.asList(
+			new Identifier("minecraft", "blocks/black_banner"),
+			new Identifier("minecraft", "blocks/red_banner"),
+			new Identifier("minecraft", "blocks/green_banner"),
+			new Identifier("minecraft", "blocks/brown_banner"),
+			new Identifier("minecraft", "blocks/blue_banner"),
+			new Identifier("minecraft", "blocks/purple_banner"),
+			new Identifier("minecraft", "blocks/cyan_banner"),
+			new Identifier("minecraft", "blocks/light_gray_banner"),
+			new Identifier("minecraft", "blocks/gray"),
+			new Identifier("minecraft", "blocks/pink_banner"),
+			new Identifier("minecraft", "blocks/lime_banner"),
+			new Identifier("minecraft", "blocks/yellow_banner"),
+			new Identifier("minecraft", "blocks/light_blue_banner"),
+			new Identifier("minecraft", "blocks/magenta_banner"),
+			new Identifier("minecraft", "blocks/orange_banner"),
+			new Identifier("minecraft", "blocks/white_banner")
+	);
 
 	@Override
 	public void onInitialize() {
@@ -31,5 +56,14 @@ public final class Bannerpp implements ModInitializer {
 		}
 
 		RegistryEntryAddedCallback.event(LOOM_PATTERN_REGISTRY).register((raw, id, p) -> LoomPatternsInternal.addPattern(p));
+		LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, lootTableId, supplier, setter) -> {
+			if (BANNER_LOOT_TABLES.contains(lootTableId)) {
+				supplier.withFunction(CopyNbtLootFunction
+						.builder(CopyNbtLootFunction.Source.BLOCK_ENTITY)
+						.withOperation(LoomPatternContainer.NBT_KEY, "BlockEntityTag." + LoomPatternContainer.NBT_KEY)
+						.build()
+				);
+			}
+		});
 	}
 }
