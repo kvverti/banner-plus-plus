e55d75889fe7216fba8773fe8b02d4af817a2173
byquanton <32410361+byquanton@users.noreply.github.com>
2022-06-07 16:03:34 +0200

    Update Dependencies and Apply Checkstyle

--- src/main/java/io/github/fablabsmc/fablabs/mixin/bannerpattern/client/BannerBlockEntityClientMixin.java
+++ src/main/java/io/github/fablabsmc/fablabs/mixin/bannerpattern/client/BannerBlockEntityClientMixin.java
@@ -7,16 +7,16 @@ import com.mojang.datafixers.util.Pair;
 import io.github.fablabsmc.fablabs.impl.bannerpattern.LoomPatternConversions;
 import io.github.fablabsmc.fablabs.impl.bannerpattern.LoomPatternData;
 import io.github.fablabsmc.fablabs.impl.bannerpattern.iface.LoomPatternContainer;
-import net.minecraft.block.entity.BannerPattern;
-import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.block.entity.BannerBlockEntity;
+import net.minecraft.block.entity.BannerPattern;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtList;
